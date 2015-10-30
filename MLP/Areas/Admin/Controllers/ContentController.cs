91a9a8728808be75d8d3756ee45500b8fd6825cb
asad1000 <asadmehboob1300@gmail.com>
2015-10-30 14:52:53 +0500

    commit
    fr

---
+++ MLP/Areas/Admin/Controllers/ContentController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MLP.Models;
+using MLP.Data;
+
+namespace MLP.Areas.Admin.Controllers
+{
+    public class ContentController : Controller
+    {
+        private MLPContext db = new MLPContext();
+
+        // GET: /Admin/Content/
+        public ActionResult Index()
+        {
+            var contents = db.Contents.Include(c => c.Module);
+            return View(contents.ToList());
+        }
+
+        // GET: /Admin/Content/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Content content = db.Contents.Find(id);
+            if (content == null)
+            {
+                return HttpNotFound();
+            }
+            return View(content);
+        }
+
+        // GET: /Admin/Content/Create
+        public ActionResult Create()
+        {
+            ViewBag.ModuleId = new SelectList(db.Modules, "ModuleId", "ModuleName");
+            return View();
+        }
+
+        // POST: /Admin/Content/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ContentId,Description,ModuleId")] Content content)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contents.Add(content);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ModuleId = new SelectList(db.Modules, "ModuleId", "ModuleName", content.ModuleId);
+            return View(content);
+        }
+
+        // GET: /Admin/Content/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Content content = db.Contents.Find(id);
+            if (content == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ModuleId = new SelectList(db.Modules, "ModuleId", "ModuleName", content.ModuleId);
+            return View(content);
+        }
+
+        // POST: /Admin/Content/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ContentId,Description,ModuleId")] Content content)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(content).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ModuleId = new SelectList(db.Modules, "ModuleId", "ModuleName", content.ModuleId);
+            return View(content);
+        }
+
+        // GET: /Admin/Content/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Content content = db.Contents.Find(id);
+            if (content == null)
+            {
+                return HttpNotFound();
+            }
+            return View(content);
+        }
+
+        // POST: /Admin/Content/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Content content = db.Contents.Find(id);
+            db.Contents.Remove(content);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
