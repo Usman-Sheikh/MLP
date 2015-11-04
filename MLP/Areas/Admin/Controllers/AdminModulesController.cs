a06cf3eca65fe14da4beb5c07c1104b9aacf52f3
Usman-Sheikh <sh.usmanikram@hotmail.com>
2015-11-04 15:02:23 +0500

    Module Added

---
+++ MLP/Areas/Admin/Controllers/AdminModulesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MLP.Data;
+using MLP.Models;
+
+namespace MLP.Areas.Admin.Controllers
+{
+    public class AdminModulesController : Controller
+    {
+        private MLPContext db = new MLPContext();
+
+        // GET: Admin/AdminModules
+        public ActionResult Index()
+        {
+            var modules = db.Modules.Include(m => m.Course);
+            return View(modules.ToList());
+        }
+
+        // GET: Admin/AdminModules/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Module module = db.Modules.Find(id);
+            if (module == null)
+            {
+                return HttpNotFound();
+            }
+            return View(module);
+        }
+
+        // GET: Admin/AdminModules/Create
+        public ActionResult Create()
+        {
+            ViewBag.CourseId = new SelectList(db.Courses, "CourseId", "Title");
+            return View();
+        }
+
+        // POST: Admin/AdminModules/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ModuleId,ModuleName,CourseId")] Module module)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Modules.Add(module);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CourseId = new SelectList(db.Courses, "CourseId", "Title", module.CourseId);
+            return View(module);
+        }
+
+        // GET: Admin/AdminModules/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Module module = db.Modules.Find(id);
+            if (module == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CourseId = new SelectList(db.Courses, "CourseId", "Title", module.CourseId);
+            return View(module);
+        }
+
+        // POST: Admin/AdminModules/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ModuleId,ModuleName,CourseId")] Module module)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(module).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CourseId = new SelectList(db.Courses, "CourseId", "Title", module.CourseId);
+            return View(module);
+        }
+
+        // GET: Admin/AdminModules/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Module module = db.Modules.Find(id);
+            if (module == null)
+            {
+                return HttpNotFound();
+            }
+            return View(module);
+        }
+
+        // POST: Admin/AdminModules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Module module = db.Modules.Find(id);
+            db.Modules.Remove(module);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
